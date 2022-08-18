7057f986334eedd878eabdc2ba8cc25596523ba7
angelina <angelina.i612@mail.ru>
2022-08-19 00:41:22 +0800

    Add some code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const mySwiper = new Swiper(".swiper-container222", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 40,
+  slidesPerView: 4,
+  loopFillGroupWithBlank: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
